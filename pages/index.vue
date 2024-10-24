b547542f4a394a9b4b411cfbdfbca5cd1d693193
Madao <gn00678465@gmail.com>
2024-10-25 01:22:01 +0800

    feat: 新增 Typography 設定

--- pages/index.vue
+++ pages/index.vue
@@ -16,8 +16,21 @@ const route = useRoute()
     <div class="text-white bg-success">success</div>
     <div class="text-white bg-warning">warning</div>
     <div class="text-white bg-error">error</div>
-    <br>
     <div class="bg-body text-white">background-body</div>
+    <br>
+    <p class="text-display">display</p>
+    <h1 class="text-h1">heading-1</h1>
+    <h2 class="text-h2">heading-2</h2>
+    <h3 class="text-h3">heading-3</h3>
+    <h4 class="text-h4">heading-4</h4>
+    <h5 class="text-h5">heading-5</h5>
+    <h6 class="text-h6">heading-6</h6>
+    <p class="text-title">Title</p>
+    <p class="text-subtitle">Subtitle</p>
+    <p class="text-body">Body</p>
+    <p class="text-body2">Body2</p>
+    <p class="text-tiny">tiny</p>
+
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,63 @@
 import { defineConfig, presetUno } from 'unocss'
 import { presetDarkModeTheme } from '@nuxt-enjoyment-luxury-hotel/uno-preset'
+import presetWebFonts from 'unocss/preset-web-fonts'
 import { luxuryHotelTheme } from './build/luxury-hotel-theme'
 
 export default defineConfig({
   presets: [
     presetUno({ dark: 'class' }),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Noto Sans TC',
+        mono: ['Fira Code', 'Fira Mono:400,700'],
+      },
+    }),
     presetDarkModeTheme({
       theme: luxuryHotelTheme,
       variablePrefix: 'un-'
     }),
   ],
+  preflights: [
+    {
+      getCSS() {
+        return `
+          html {
+            fontFamily: "Noto Sans TC", sans-serif, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            font-weight: 500;
+            letter-spacing: 0.02em;
+            line-height: 1.5;
+          }
+          :where(:root) {
+            font-size: 16px;
+          }
+          h1,h2,h3,h4,h5,h6 {
+            font-weight: 700;
+            letter-spacing: 0.05em;
+            line-height: 1.2;
+          }
+        `
+      }
+    }
+  ],
+  rules: [
+    [/^m-(\d+)$/, ([, d]) => ({ margin: `${parseInt(d) / 4}rem` })],
+    [/^p-(\d+)$/, match => ({ padding: `${parseInt(match[1]) / 4}rem` })],
+  ],
+  theme: {
+    fontSize: {
+      tiny: [`${12/16}rem`, { 'font-weight': 500 }],
+      body2: [`${14/16}rem`, { 'font-weight': 500 }],
+      body: [`${16/16}rem`, { 'font-weight': 500 }],
+      subtitle: [`${14/16}rem`, { 'font-weight': 700 }],
+      title: [`${16/16}rem`, { 'font-weight': 700 }],
+      h6: [`${20/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+      h5: [`${24/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+      h4: [`${28/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+      h3: [`${32/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+      h2: [`${40/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+      h1: [`${48/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+      display: [`${100/16}rem`, { 'font-weight': 700, 'letter-spacing': '0.05em', 'line-height': 1.2 }],
+    }
+  }
 })
