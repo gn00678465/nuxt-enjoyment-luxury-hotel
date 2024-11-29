bbdd746b9387d2d91c5dec1d2e184a57ebb1e982
madao <gn00678465@gmail.com>
2024-11-29 08:15:41 +0800

    chore: add pinia module

--- apps/bootstrap/nuxt.config.ts
+++ apps/bootstrap/nuxt.config.ts
@@ -22,7 +22,8 @@ export default defineNuxtConfig({
       },
     }],
     'nuxt-viewport',
-    '@samk-dev/nuxt-vcalendar'
+    '@samk-dev/nuxt-vcalendar',
+    '@pinia/nuxt'
   ],
   extends: [
     'assets'
